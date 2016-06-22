d084738ba5db29daced6c91a665177009ef69c53
kasicass <kasicass@gmail.com>
2016-06-22 15:43:03 +0800

    gl - ProjShadow

---
+++ gl/src/01_ProjShadow/main.c
@@ -0,0 +1,460 @@
+/*  project texture demo
+ *
+ *      written by Alexander Zaprjagaev
+ *      frustum@public.tsu.ru
+ *      2:5005/93.15@FidoNet
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <GL/glut.h>
+#include "mathlib.h"
+
+unsigned char *LoadJPEG(char*,int*,int*);
+float *Load3DS(char*,int*);
+
+int findcrosspoint(const float *a,const float *b,float *point);
+
+#define SHADOWSIZE 256
+#define MODELSIZE 32
+
+int groundlist;
+int modellist;
+int lightlist;
+
+float lightpos[4];
+float objpos[3];
+float camerapos[3];
+float cameradir[3];
+
+int stop;
+int view;
+float dist = 100;
+float alpha = 60;
+
+float modelangle;
+float modelsize;
+float *vertexmodel;
+int num_vertexmodel;
+
+float *vertexground;
+float *planeground;
+int num_vertexground;
+
+int textureground;
+int textureshadow;
+int texturemodel;
+
+void init(void) {
+    float tmp,xmin,xmax,ymin,ymax,zmin,zmax,a[3],b[3];
+    int i,j,k,width,height;
+    unsigned char *data;
+    float plane_S[] = { 1.0, 0.0, 0.0, 0.0 };
+    float plane_T[] = { 0.0, 1.0, 0.0, 0.0 };
+    float plane_R[] = { 0.0, 0.0, 1.0, 0.0 };
+    float plane_Q[] = { 0.0, 0.0, 0.0, 1.0 };
+
+    glClearDepth(1.0);
+    glShadeModel(GL_SMOOTH);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glDisable(GL_CULL_FACE);
+    
+    glEnable(GL_LIGHT0);
+    
+    modelsize = 0;
+    vertexmodel = Load3DS("data/model.3ds",&num_vertexmodel);
+    printf("./data/model.3ds %d face\n",num_vertexmodel / 3);
+    
+    xmin = xmax = ymin = ymax = zmin = zmax = 0;
+    
+    for(i = 0; i < num_vertexmodel; i++) {
+        if(vertexmodel[(i << 3) + 0] < xmin) xmin = vertexmodel[(i << 3) + 0];
+        if(vertexmodel[(i << 3) + 0] > xmax) xmax = vertexmodel[(i << 3) + 0];
+        if(vertexmodel[(i << 3) + 1] < ymin) ymin = vertexmodel[(i << 3) + 1];
+        if(vertexmodel[(i << 3) + 1] > ymax) ymax = vertexmodel[(i << 3) + 1];
+        if(vertexmodel[(i << 3) + 2] < zmin) zmin = vertexmodel[(i << 3) + 2];
+        if(vertexmodel[(i << 3) + 2] > zmax) zmax = vertexmodel[(i << 3) + 2];
+    }
+    
+    for(i = 0; i < num_vertexmodel; i++) {
+        vertexmodel[(i << 3) + 0] -= (xmax + xmin) / 2.0;
+        vertexmodel[(i << 3) + 1] -= (ymax + ymin) / 2.0;
+        vertexmodel[(i << 3) + 2] -= (zmax + zmin) / 2.0;
+    }
+    
+    for(i = 0; i < num_vertexmodel; i++) {
+        tmp = sqrt(vertexmodel[(i << 3) + 0] * vertexmodel[(i << 3) + 0] +
+                    vertexmodel[(i << 3) + 1] * vertexmodel[(i << 3) + 1] +
+                    vertexmodel[(i << 3) + 2] * vertexmodel[(i << 3) + 2]);
+        if(tmp > modelsize) modelsize = tmp;
+    }
+    
+    tmp = MODELSIZE / modelsize;
+    modelsize = MODELSIZE;
+    for(i = 0; i < num_vertexmodel; i++) {
+        vertexmodel[(i << 3) + 0] *= tmp;
+        vertexmodel[(i << 3) + 1] *= tmp;
+        vertexmodel[(i << 3) + 2] *= tmp;
+    }
+    
+    vertexground = Load3DS("data/ground.3ds",&num_vertexground);
+    printf("./data/ground.3ds %d face\n",num_vertexground / 3);
+    
+    planeground = (float*)malloc(sizeof(float) * 4 * num_vertexground / 3);
+    for(i = 0, j = 0, k = 0; i < num_vertexground; i += 3, j += 24, k += 4) {
+        VectorSub(&vertexground[j + 8],&vertexground[j],a);
+        VectorSub(&vertexground[j + 16],&vertexground[j],b);
+        VectorCrossProduct(a,b,&planeground[k]);
+        VectorNormalize(&planeground[k],&planeground[k]);
+        planeground[k + 3] = -VectorDotProduct(&planeground[k],&vertexground[j]);
+    }
+    
+    glGenTextures(1,&textureground);
+    glBindTexture(GL_TEXTURE_2D,textureground);
+    if((data = LoadJPEG("data/ground.jpg",&width,&height))) {
+        glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
+        glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
+        gluBuild2DMipmaps(GL_TEXTURE_2D,4,width,height,GL_RGBA,GL_UNSIGNED_BYTE,data);
+        free(data);
+    }
+    
+    glGenTextures(1,&texturemodel);
+    glBindTexture(GL_TEXTURE_2D,texturemodel);
+    if((data = LoadJPEG("data/model.jpg",&width,&height))) {
+        glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
+        glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
+        gluBuild2DMipmaps(GL_TEXTURE_2D,4,width,height,GL_RGBA,GL_UNSIGNED_BYTE,data);
+        free(data);
+    }
+    
+    glGenTextures(1,&textureshadow);
+    glBindTexture(GL_TEXTURE_2D,textureshadow);
+    glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_CLAMP);
+    glTexImage2D(GL_TEXTURE_2D,0,4,SHADOWSIZE,SHADOWSIZE,0,GL_RGBA,GL_UNSIGNED_BYTE,NULL);
+
+    glTexGenfv(GL_S,GL_EYE_PLANE,plane_S);
+    glTexGenfv(GL_T,GL_EYE_PLANE,plane_T);
+    glTexGenfv(GL_R,GL_EYE_PLANE,plane_R);
+    glTexGenfv(GL_Q,GL_EYE_PLANE,plane_Q);
+
+    modellist = glGenLists(1);
+    glNewList(modellist,GL_COMPILE);
+    glPushMatrix();
+    glBegin(GL_TRIANGLES);
+    for(i = 0; i < num_vertexmodel; i++) {
+        glNormal3fv((float*)&vertexmodel[i << 3] + 3);
+        glTexCoord2fv((float*)&vertexmodel[i << 3] + 6);
+        glVertex3fv((float*)&vertexmodel[i << 3]);
+    }
+    glEnd();
+    glPopMatrix();
+    glEndList();
+    
+    groundlist = glGenLists(1);
+    glNewList(groundlist,GL_COMPILE);
+    glBegin(GL_TRIANGLES);
+    for(i = 0; i < num_vertexground; i++) {
+        glNormal3fv((float*)&vertexground[i << 3] + 3);
+        glTexCoord2fv((float*)&vertexground[i << 3] + 6);
+        glVertex3fv((float*)&vertexground[i << 3]);
+    }
+    glEnd();
+    glEndList();
+    
+    lightlist = glGenLists(1);
+    glNewList(lightlist,GL_COMPILE);
+    glutSolidSphere(0.6,16,16);
+    glEndList();
+}
+
+void shadowmap(void) {
+    glViewport(0,0,SHADOWSIZE,SHADOWSIZE);
+    glClearColor(0,0,0,1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-modelsize,modelsize,-modelsize,modelsize,1,10000);
+    glMatrixMode(GL_TEXTURE);
+    glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(lightpos[0],lightpos[1],lightpos[2], objpos[0],objpos[1],objpos[2], 0,1,0);
+    
+    glDisable(GL_LIGHTING);
+    glDisable(GL_TEXTURE_2D);
+    
+    glColor4f(0.5,0.5,0.5,1);
+
+    glPushMatrix();
+    glTranslatef(objpos[0],objpos[1],objpos[2]);
+    glRotatef(modelangle,0,0,1);
+    glCallList(modellist);
+    glPopMatrix();
+
+    glBindTexture(GL_TEXTURE_2D,textureshadow);
+    glCopyTexSubImage2D(GL_TEXTURE_2D,0,0,0,0,0,SHADOWSIZE,SHADOWSIZE);
+    
+    glColor4f(1,1,1,1);
+    
+    glViewport(0,0,800,600);
+}
+
+float getfps() {
+    static int start,end,count;
+    static float fps;
+    if(count > 10) {
+        end = start;
+        start = glutGet(GLUT_ELAPSED_TIME);
+        fps = count * 1000.0 / (start - end);
+        count = 0;
+    }
+    count++;
+    return fps;
+}
+
+void    drawstring(float x,float y,char *string) {
+    int i,l;
+    glRasterPos2f(x,y);
+    l = strlen(string);
+    for(i = 0; i < l; i++)
+        glutBitmapCharacter(GLUT_BITMAP_8_BY_13,string[i]);
+}
+
+void display(void) {
+    float mview[16],imview[16];
+    char buffer[128];
+
+    shadowmap();
+
+    glClearColor(0,0,0,1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45,4.0 / 3.0,1,2048);
+    
+    glMatrixMode(GL_TEXTURE);
+    glLoadIdentity();
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(camerapos[0],camerapos[1],camerapos[2], cameradir[0],cameradir[1],cameradir[2], 0,0,1);
+
+    glLightfv(GL_LIGHT0,GL_POSITION,lightpos);
+
+    glEnable(GL_TEXTURE_GEN_S);
+    glEnable(GL_TEXTURE_GEN_T);
+    glTexGeni(GL_S,GL_TEXTURE_GEN_MODE,GL_SPHERE_MAP);
+    glTexGeni(GL_T,GL_TEXTURE_GEN_MODE,GL_SPHERE_MAP);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_TEXTURE_2D);                        // draw model
+    glBindTexture(GL_TEXTURE_2D,texturemodel);
+    glPushMatrix();
+    glTranslatef(objpos[0],objpos[1],objpos[2]);
+    glRotatef(modelangle,0,0,1);
+    glCallList(modellist);
+    glPopMatrix();
+    
+    glDisable(GL_TEXTURE_GEN_S);
+    glDisable(GL_TEXTURE_GEN_T);
+
+    glDisable(GL_LIGHTING);
+    glBindTexture(GL_TEXTURE_2D,textureground);     // draw ground
+    glCallList(groundlist);
+
+    glDisable(GL_TEXTURE_2D);
+    glPushMatrix();                                 // light sources
+    glTranslatef(lightpos[0],lightpos[1],lightpos[2]);
+    glColor4f(0,1,0,1);
+    glCallList(lightlist);
+    glColor4f(1,1,1,1);
+    glPopMatrix();
+    
+    glEnable(GL_TEXTURE_GEN_S);
+    glEnable(GL_TEXTURE_GEN_T);
+    glEnable(GL_TEXTURE_GEN_R);
+    glEnable(GL_TEXTURE_GEN_Q);
+    glTexGeni(GL_S,GL_TEXTURE_GEN_MODE,GL_EYE_LINEAR);
+    glTexGeni(GL_T,GL_TEXTURE_GEN_MODE,GL_EYE_LINEAR);
+    glTexGeni(GL_R,GL_TEXTURE_GEN_MODE,GL_EYE_LINEAR);
+    glTexGeni(GL_Q,GL_TEXTURE_GEN_MODE,GL_EYE_LINEAR);
+
+    glGetFloatv(GL_MODELVIEW_MATRIX,mview);
+    MatrixInverse(mview,imview);
+
+    glMatrixMode(GL_TEXTURE);
+    glLoadIdentity();
+    glTranslatef(0.5,0.5,0);
+    glScalef(0.5,0.5,1.0);
+    glOrtho(-modelsize,modelsize,-modelsize,modelsize,-1,1);
+    gluLookAt(lightpos[0],lightpos[1],lightpos[2], objpos[0],objpos[1],objpos[2], 0,1,0);
+    glMultMatrixf(imview);
+
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_BLEND);                             // shadow
+    glBlendFunc(GL_ZERO,GL_ONE_MINUS_SRC_COLOR);
+    glBindTexture(GL_TEXTURE_2D,textureshadow);
+    glCallList(groundlist);
+    glDisable(GL_BLEND);
+    
+    glMatrixMode(GL_MODELVIEW);
+    glDisable(GL_TEXTURE_GEN_S);
+    glDisable(GL_TEXTURE_GEN_T);
+    glDisable(GL_TEXTURE_GEN_R);
+    glDisable(GL_TEXTURE_GEN_Q);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1,1,-1,1,-1,1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glColor4f(1,1,1,1);
+    glDisable(GL_DEPTH_TEST);
+    glDisable(GL_TEXTURE_2D);
+    glDisable(GL_LIGHTING);
+    sprintf(buffer,"fps: %.2f",getfps());
+    drawstring(-0.95,0.95,buffer);
+    glEnable(GL_DEPTH_TEST);
+    
+    glutSwapBuffers();
+}
+
+int findcrosspoint(const float *a,const float *b,float *point) {
+    register int i,j,k;
+    float dot0,dot1,angle,p0[3],p1[3],p2[3];
+    for(i = 0, j = 0, k = 0; i < num_vertexground; i += 3, j += 4, k += 24) {
+        dot0 = -VectorDotProduct(a,&planeground[j]);
+        dot1 = -VectorDotProduct(b,&planeground[j]);
+        VectorSub(b,a,point);
+        VectorScale(point,(planeground[j + 3] - dot0) / (dot1 - dot0),point);
+        VectorAdd(point,a,point);
+        VectorSub(point,&vertexground[k],p0);
+        VectorSub(point,&vertexground[k + 8],p1);
+        VectorSub(point,&vertexground[k + 16],p2);
+        VectorNormalize(p0,p0);
+        VectorNormalize(p1,p1);
+        VectorNormalize(p2,p2);
+        angle = acos(VectorDotProduct(p0,p1)) +
+                acos(VectorDotProduct(p1,p2)) +
+                acos(VectorDotProduct(p2,p0));
+        if(angle > 6.28) return 1;
+    }
+    return 0;
+}
+
+int testpolygon(const float *v1,const float *v2,const float *v3,const float *p) {
+    if((v2[0] - v1[0]) * (p[1] - v1[1]) - (v2[1] - v1[1]) * (p[0] - v1[0]) < 0.0 ||
+        (v3[0] - v2[0]) * (p[1] - v2[1]) - (v3[1] - v2[1]) * (p[0] - v2[0]) < 0.0 ||
+        (v1[0] - v3[0]) * (p[1] - v3[1]) - (v1[1] - v3[1]) * (p[0] - v3[0]) < 0.0) return 0;
+    return 1;
+}
+
+float heightground(const float *point) {
+    register int i,j;
+    float k,dot0,dot1,a[3],b[3],plane[4];
+    for(i = 0, j = 0; i < num_vertexground; i += 3, j += 24)
+        if(testpolygon(&vertexground[j],&vertexground[j + 8],&vertexground[j + 16],point)) break;
+    VectorCopy(&planeground[j / 6],plane);
+    plane[3] = planeground[j / 6 + 3];
+    VectorCopy(point,a);
+    VectorCopy(point,b);
+    b[2] += 1.0;
+    dot0 = -VectorDotProduct(plane,a);
+    dot1 = -VectorDotProduct(plane,b);
+    k = (plane[3] - dot0) / (dot1 - dot0);
+    return a[2] + (b[2] - a[2]) * k;
+}
+
+void idle(void) {
+    static float angle0,angle1;
+    objpos[0] = 100 * cos(angle0);
+    objpos[1] = 100 * sin(angle0);
+    objpos[2] = heightground(objpos) + 60;
+    modelangle = angle0 * RAD2DEG;
+    lightpos[0] = objpos[0] + 10 * cos(angle1);
+    lightpos[1] = objpos[1] + 10 * sin(angle1);
+    lightpos[2] = objpos[2] + 40;
+    lightpos[3] = 1.0;
+    if(view) {
+        VectorSet(0,0,0,cameradir);
+    } else {
+        VectorCopy(objpos,cameradir);
+    }
+    camerapos[0] = cameradir[0] + dist * sin(3.14 * alpha / 180.0);
+    camerapos[1] = cameradir[1] + 10;
+    camerapos[2] = cameradir[2] + dist * cos(3.14 * alpha / 180.0);
+    switch(stop) {
+        case 0:
+            angle0 += 0.004;
+            angle1 -= 0.016;
+            break;
+        case 1:
+            angle1 += 0.016;
+            break;
+        case 2:
+            angle0 += 0.004;
+            break;
+        case 3:
+            break;
+    }
+    glutPostRedisplay();
+}
+
+void keyboard(unsigned char c,int x,int y) {
+    switch(c) {
+        case 0x1b:
+            exit(1);
+            break;
+        case 'm':
+            stop++;
+            if(stop == 4) stop = 0;
+            break;
+        case 'a':
+            dist -= 5;
+            break;
+        case 'z':
+            dist += 5;
+            break;
+        case 's':
+            alpha--;
+            break;
+        case 'x':
+            alpha++;
+            break;
+        case 'v':
+            view++;
+            if(view == 2) view = 0;
+            break;
+    }
+}
+
+int main(int argc,char **argv) {
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+
+    //glutGameModeString("800x600");
+    //glutEnterGameMode();
+    //glutSetCursor(GLUT_CURSOR_NONE);
+
+	glutInitWindowSize(800, 600);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("01 - Project Shadow");
+
+    init();
+    glutDisplayFunc(display);
+    glutIdleFunc(idle);
+    glutKeyboardFunc(keyboard);
+    glutMainLoop();
+    return 0;
+}
---
+++ gl/src/01_ProjShadow/mathlib.c
@@ -0,0 +1,155 @@
+/*  3d math lib
+ *
+ *      written by Alexander Zaprjagaev
+ *      frustum@public.tsu.ru
+ *      2:5005/93.15@FidoNet
+ */
+
+#include <math.h>
+#include "mathlib.h"
+
+float   VectorLength(const float *v) {
+    return sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+}
+
+float   VectorNormalize(const float *v,float *out) {
+    float length,ilength;
+    length = sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+    if(length == 0) {
+        VectorClear(out);
+        return 0;
+    }
+    ilength = 1.0 / length;
+    VectorScale(v,ilength,out);
+    return length;
+}
+
+void    VectorCrossProduct(const float *v1,const float *v2,float *out) {
+    out[0] = v1[1] * v2[2] - v1[2] * v2[1];
+    out[1] = v1[2] * v2[0] - v1[0] * v2[2];
+    out[2] = v1[0] * v2[1] - v1[1] * v2[0];
+}
+
+void    VectorTransform(const float *v,const float *m,float *out) {
+    float v1[3];
+    VectorCopy(v,v1);
+    out[0] = v1[0] * m[0] + v1[1] * m[4] + v1[2] * m[8] + m[12];
+    out[1] = v1[0] * m[1] + v1[1] * m[5] + v1[2] * m[9] + m[13];
+    out[2] = v1[0] * m[2] + v1[1] * m[6] + v1[2] * m[10] + m[14];
+}
+
+void    VectorTransformNormal(const float *v,const float *m,float *out) {
+    float v1[3];
+    VectorCopy(v,v1);
+    out[0] = v1[0] * m[0] + v1[1] * m[4] + v1[2] * m[8];
+    out[1] = v1[0] * m[1] + v1[1] * m[5] + v1[2] * m[9];
+    out[2] = v1[0] * m[2] + v1[1] * m[6] + v1[2] * m[10];
+}
+
+void    MatrixMultiply(const float *m1,const float *m2,float *out) {
+    out[0] = m1[0] * m2[0] + m1[4] * m2[1] + m1[8] * m2[2] + m1[12] * m2[3];
+    out[1] = m1[1] * m2[0] + m1[5] * m2[1] + m1[9] * m2[2] + m1[13] * m2[3];
+    out[2] = m1[2] * m2[0] + m1[6] * m2[1] + m1[10] * m2[2] + m1[14] * m2[3];
+    out[3] = m1[3] * m2[0] + m1[7] * m2[1] + m1[11] * m2[2] + m1[15] * m2[3];
+    out[4] = m1[0] * m2[4] + m1[4] * m2[5] + m1[8] * m2[6] + m1[12] * m2[7];
+    out[5] = m1[1] * m2[4] + m1[5] * m2[5] + m1[9] * m2[6] + m1[13] * m2[7];
+    out[6] = m1[2] * m2[4] + m1[6] * m2[5] + m1[10] * m2[6] + m1[14] * m2[7];
+    out[7] = m1[3] * m2[4] + m1[7] * m2[5] + m1[11] * m2[6] + m1[15] * m2[7];
+    out[8] = m1[0] * m2[8] + m1[4] * m2[9] + m1[8] * m2[10] + m1[12] * m2[11];
+    out[9] = m1[1] * m2[8] + m1[5] * m2[9] + m1[9] * m2[10] + m1[13] * m2[11];
+    out[10] = m1[2] * m2[8] + m1[6] * m2[9] + m1[10] * m2[10] + m1[14] * m2[11];
+    out[11] = m1[3] * m2[8] + m1[7] * m2[9] + m1[11] * m2[10] + m1[15] * m2[11];
+    out[12] = m1[0] * m2[12] + m1[4] * m2[13] + m1[8] * m2[14] + m1[12] * m2[15];
+    out[13] = m1[1] * m2[12] + m1[5] * m2[13] + m1[9] * m2[14] + m1[13] * m2[15];
+    out[14] = m1[2] * m2[12] + m1[6] * m2[13] + m1[10] * m2[14] + m1[14] * m2[15];
+    out[15] = m1[3] * m2[12] + m1[7] * m2[13] + m1[11] * m2[14] + m1[15] * m2[15];    
+}
+
+int MatrixInverse(const float *m,float *out) {
+    float   det;
+    det = m[0] * m[5] * m[10];
+    det += m[4] * m[9] * m[2];
+    det += m[8] * m[1] * m[6];
+    det -= m[8] * m[5] * m[2];
+    det -= m[4] * m[1] * m[10];
+    det -= m[0] * m[9] * m[6];
+    if(det * det < 1e-25) return 0;
+    det = 1.0 / det;    
+    out[0] =    (m[5] * m[10] - m[9] * m[6]) * det;
+    out[1] =  - (m[1] * m[10] - m[9] * m[2]) * det;
+    out[2] =    (m[1] * m[6] -  m[5] * m[2]) * det;
+    out[3] = 0.0;
+    out[4] =  - (m[4] * m[10] - m[8] * m[6]) * det;
+    out[5] =    (m[0] * m[10] - m[8] * m[2]) * det;
+    out[6] =  - (m[0] * m[6] -  m[4] * m[2]) * det;
+    out[7] = 0.0;
+    out[8] =    (m[4] * m[9] -  m[8] * m[5]) * det;
+    out[9] =  - (m[0] * m[9] -  m[8] * m[1]) * det;
+    out[10] =   (m[0] * m[5] -  m[4] * m[1]) * det;
+    out[11] = 0.0;
+    out[12] = - (m[12] * out[0] + m[13] * out[4] + m[14] * out[8]);
+    out[13] = - (m[12] * out[1] + m[13] * out[5] + m[14] * out[9]);
+    out[14] = - (m[12] * out[2] + m[13] * out[6] + m[14] * out[10]);
+    out[15] = 1.0;
+    return 1;
+}
+
+void    MatrixTranspose(const float *m,float *out) {
+   out[0] = m[0]; out[4] = m[1]; out[8] = m[2]; out[12] = m[3];
+   out[1] = m[4]; out[5] = m[5]; out[9] = m[6]; out[13] = m[7];
+   out[2] = m[8]; out[6] = m[9]; out[10] = m[10]; out[14] = m[11];
+   out[3] = m[12]; out[7] = m[13]; out[11] = m[14]; out[15] = m[15];
+}
+
+void    MatrixRotationX(float angle,float *out) {
+    float rad = angle * DEG2RAD;
+    float Cos = cos(rad);
+    float Sin = sin(rad);
+    out[0] = 1.0; out[4] = 0.0; out[8] = 0.0; out[12] = 0.0;
+    out[1] = 0.0; out[5] = Cos; out[9] = -Sin; out[13] = 0.0;
+    out[2] = 0.0; out[6] = Sin; out[10] = Cos; out[14] = 0.0;
+    out[3] = 0.0; out[7] = 0.0; out[11] = 0.0; out[15] = 1.0;
+}
+
+void    MatrixRotationY(float angle,float *out) {
+    float rad = angle * DEG2RAD;
+    float Cos = cos(rad);
+    float Sin = sin(rad);
+    out[0] = Cos; out[4] = 0.0; out[8] = Sin; out[12] = 0.0;
+    out[1] = 0.0; out[5] = 1.0; out[9] = 0.0; out[13] = 0.0;
+    out[2] = -Sin; out[6] = 0.0; out[10] = Cos; out[14] = 0.0;
+    out[3] = 0.0; out[7] = 0.0; out[11] = 0.0; out[15] = 1.0;
+}
+
+void    MatrixRotationZ(float angle,float *out) {
+    float rad = angle * DEG2RAD;
+    float Cos = cos(rad);
+    float Sin = sin(rad);
+    out[0] = Cos; out[4] = -Sin; out[8] = 0.0; out[12] = 0.0;
+    out[1] = Sin; out[5] = Cos; out[9] = 0.0; out[13] = 0.0;
+    out[2] = 0.0; out[6] = 0.0; out[10] = 1.0; out[14] = 0.0;
+    out[3] = 0.0; out[7] = 0.0; out[11] = 0.0; out[15] = 1.0;
+}
+
+void    MatrixTranslate(float x,float y,float z,float *out) {
+    out[0] = 1.0; out[4] = 0.0; out[8] = 0.0; out[12] = x;
+    out[1] = 0.0; out[5] = 1.0; out[9] = 0.0; out[13] = y;
+    out[2] = 0.0; out[6] = 0.0; out[10] = 1.0; out[14] = z;
+    out[3] = 0.0; out[7] = 0.0; out[11] = 0.0; out[15] = 1.0;
+}
+
+void    MatrixLookAt(const float *eye,const float *dir,const float *up,float *out) {
+    float x[3],y[3],z[3],m1[16],m2[16];
+    VectorSub(eye,dir,z);
+    VectorNormalize(z,z);
+    VectorCrossProduct(up,z,x);
+    VectorCrossProduct(z,x,y);
+    VectorNormalize(x,x);
+    VectorNormalize(y,y);
+    m1[0] = x[0]; m1[4] = x[1]; m1[8] = x[2]; m1[12] = 0.0;
+    m1[1] = y[0]; m1[5] = y[1]; m1[9] = y[2]; m1[13] = 0.0;
+    m1[2] = z[0]; m1[6] = z[1]; m1[10] = z[2]; m1[14] = 0.0;
+    m1[3] = 0.0; m1[7] = 0.0; m1[11] = 0.0; m1[15] = 1.0;
+    MatrixTranslate(-eye[0],-eye[1],-eye[2],m2);
+    MatrixMultiply(m1,m2,out);
+}
